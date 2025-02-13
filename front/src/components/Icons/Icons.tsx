2351bf98e8ff4bbbe9d6324abef3ff2a1cec64e1
Vasile Grinciuc <raptordias@gmail.com>
2025-02-13 20:55:31 +0200

    Split components

--- front/src/components/Icons/Icons.tsx
+++ front/src/components/Icons/Icons.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 export const WardrobeIcon = () => (
   <Image
-    width="200"
-    height="200"
+    width="25"
+    height="25"
     src="/wardrobe-button.svg"
     alt="Dulap.md Logo"
   />
