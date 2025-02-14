3f9fc342c1c7610543a4e4aec84b4b9cbd2af6f8
Vasile Grinciuc <raptordias@gmail.com>
2025-02-14 15:22:13 +0200

    Add product page

---
+++ front/pages/product.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import { Landing } from '~/components/Landing/Landing'
+import './styles.css'
+const Index: FC = () => {
+  return (
+    <>
+      <Landing />
+    </>
+  )
+}
+
+export default Index
---
+++ front/src/components/ProductPage/ProductPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+interface ProductItemProps {
+  button: React.ReactNode
+}
+export const ProductPage: React.FC<ProductItemProps> = ({ button }) => {
+  return <>Product page</>
+}
