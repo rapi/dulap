2ebd3225ce22a1198303491af4a3e0b8fffbedcd
Elizabet V. Grinciuc <EVG2@bnm.md>
2025-02-17 17:43:13 +0200

    product page changes

--- front/src/components/ProductPage/ProductPage.tsx
+++ front/src/components/ProductPage/ProductPage.tsx
@@ -6,6 +6,7 @@ import { Slider } from '~/components/Slider/Slider'
 import SelectColor from '~/components/SelectColor/SelectColor'
 import { ImageSelect } from '~/components/ImageSelect/ImageSelect'
 import Select from '~/components/Select/Select'
+import InfoIcon from '@mui/icons-material/Info';
 
 export const ProductPage: React.FC = () => {
   return (
@@ -19,51 +20,71 @@ export const ProductPage: React.FC = () => {
       />
       <div className={styles.container}>
         {/* Left Side: Image */}
-        <div className={styles.imageContainer}>
-          <img src="/wardrobe.jpg" alt="Wardrobe" className={styles.image} />
+        <div className={styles.imagePriceContainer}>
+          <div className={styles.imageContainer}>
+            <img src="/wardrobe.jpg" alt="Wardrobe" className={styles.image} />
+          </div>
+          <div className={styles.priceContainer}>
+            <div className={styles.priceTitle}>
+              <h4>Calculator de preț:</h4>
+              <button className={styles.infoButton}>
+                  <InfoIcon 
+                    color='action'
+                    sx={{ fontSize: 20 }}
+                  />
+              </button>
+            </div>
+            <div className={styles.price}>
+              <h2>6850 MDL</h2>
+            </div>
+          </div>
         </div>
+        
 
         {/* Right Side: Product Details */}
         <div className={styles.detailsContainer}>
           <h1 className={styles.title}>Dulap pentru haine</h1>
 
           <ButtonSelect
-            options={['Option 1', 'Option 2', 'Option 3']}
-            defaultSelected={'Option 1'}
+            options={['2 secții', '3 secții', '4 secții']}
+            defaultSelected={'2 secții'}
             onChange={() => {}}
           />
           {/* Dimensions Sliders */}
           <div className={styles.dimensions}>
-            <label className={styles.dimensionLabel}>
-              Lățime (mm) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              <Slider
-                min={1000}
-                max={5000}
-                step={5}
-                value={100}
-                onChange={(newValue) => console.log(newValue)}
-              />
-            </label>
-            <label className={styles.dimensionLabel}>
-              Înălțime (mm)&nbsp;&nbsp;&nbsp;&nbsp;
-              <Slider
-                min={1000}
-                max={5000}
-                step={5}
-                value={100}
-                onChange={(newValue) => console.log(newValue)}
-              />
-            </label>
-            <label className={styles.dimensionLabel}>
-              Adâncime (mm)
-              <Slider
-                min={1000}
-                max={5000}
-                step={5}
-                value={100}
-                onChange={(newValue) => console.log(newValue)}
-              />
-            </label>
+            <h3>Dimensiuni</h3>
+            <div className={styles.dimensionsGrid}>
+              <label className={styles.dimensionLabel}>
+                Lățime (mm) &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <Slider
+                  min={1000}
+                  max={5000}
+                  step={5}
+                  value={1000}
+                  onChange={(newValue) => console.log(newValue)}
+                />
+              </label>
+              <label className={styles.dimensionLabel}>
+                Înălțime (mm)&nbsp;&nbsp;&nbsp;&nbsp;
+                <Slider
+                  min={1000}
+                  max={5000}
+                  step={5}
+                  value={1000}
+                  onChange={(newValue) => console.log(newValue)}
+                />
+              </label>
+              <label className={styles.dimensionLabel}>
+                Adâncime (mm)
+                <Slider
+                  min={1000}
+                  max={5000}
+                  step={5}
+                  value={1000}
+                  onChange={(newValue) => console.log(newValue)}
+                />
+              </label>
+            </div>
           </div>
 
           {/* Color Selection */}
@@ -72,6 +93,7 @@ export const ProductPage: React.FC = () => {
             <SelectColor
               colors={['#eeeeee', '#b5b5b5', '#d7d0c5']}
               onChange={() => {}}
+              size='large'
             />
           </div>
 
@@ -91,9 +113,14 @@ export const ProductPage: React.FC = () => {
 
           {/* Furniture Options */}
           <div className={styles.furniture}>
+            <h3>Furnitura</h3>
             <label className={styles.furnitureLabel}>
-              Balamale:
+              <p>Balamale:</p>
               <Select options={['Standard', 'Premium', 'Deluxe']} />
+            </label>
+            <label className={styles.furnitureLabel}>
+              <p>Glisiere pentru sertare:</p>
+              <Select options={['Standard (cu bile)', 'Premium', 'Deluxe']} />
             </label>
           </div>
         </div>
