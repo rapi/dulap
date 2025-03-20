5a7a95525d38bfa9e2d83e84506424301b0da020
Vasile Grinciuc <raptordias@gmail.com>
2025-03-20 14:08:14 +0200

    Add price configurator

--- front/src/components/Select/Select.tsx
+++ front/src/components/Select/Select.tsx
@@ -1,39 +1,34 @@
 import { useState } from 'react'
+import FormControl from '@mui/material/FormControl'
+import MenuItem from '@mui/material/MenuItem'
+import MuiSelect from '@mui/material/Select'
+import * as React from 'react'
 import styles from './Select.module.css'
-
 interface SelectProps {
   options: string[]
   label?: string
 }
 
-export const Select = ({ options, label }: SelectProps) => {
+export const Select = ({ options }: SelectProps) => {
   const [selected, setSelected] = useState(options[0])
-  const [isOpen, setIsOpen] = useState(false)
-
   return (
-    <div className={styles.container}>
-      {label && <label className={styles.label}>{label}</label>}
-      <div className={styles.selectBox} onClick={() => setIsOpen(!isOpen)}>
-        <span className={styles.selected}>{selected}</span>
-        <span className={styles.arrow}>&#9662;</span>
-      </div>
-      {isOpen && (
-        <ul className={styles.options}>
-          {options.map((option) => (
-            <li
-              key={option}
-              className={styles.option}
-              onClick={() => {
-                setSelected(option)
-                setIsOpen(false)
-              }}
-            >
-              {option}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <FormControl size="small">
+      <MuiSelect
+        value={selected}
+        onChange={(event) => {
+          setSelected(event.target.value)
+        }}
+        displayEmpty
+        className={styles.select}
+        inputProps={{ 'aria-label': 'Without label' }}
+      >
+        {options.map((option) => (
+          <MenuItem key={option} value={option}>
+            <span className={styles.selectOption}>{option}</span>
+          </MenuItem>
+        ))}
+      </MuiSelect>
+    </FormControl>
   )
 }
 
