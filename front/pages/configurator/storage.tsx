81a18bfd0d539cfb43213615598132d84c04cb72
Elizabet V. Grinciuc <EVG2@bnm.md>
2025-04-02 10:31:29 +0300

    small changes

--- front/pages/configurator/storage.tsx
+++ front/pages/configurator/storage.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react'
 import { ProductPageLayout } from '~/components/ProductPageLayout/ProductPageLayout'
 import { ProductPage } from '~/components/ProductPage/ProductPage'
-import { storageProductConfiguration } from '~/components/ProductPage/productTypes/storage'
+import { StorageProductConfiguration } from '~/components/ProductPage/productTypes/storage'
 const Product: FC = () => {
   return (
     <ProductPageLayout>
-      <ProductPage components={storageProductConfiguration} name="Comodă" />
+      <ProductPage components={StorageProductConfiguration} name="Comodă" />
     </ProductPageLayout>
   )
 }
