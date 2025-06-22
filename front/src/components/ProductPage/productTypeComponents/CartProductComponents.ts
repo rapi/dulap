feb1c633f6114520f569f2d11eddbe3dd086f538
Vasile Grinciuc <raptordias@gmail.com>
2025-06-22 18:41:53 +0300

    fixes

--- front/src/components/ProductPage/productTypeComponents/CartProductComponents.ts
+++ front/src/components/ProductPage/productTypeComponents/CartProductComponents.ts
@@ -2,6 +2,10 @@ import { ProductComponent as WardrobePC } from '~/components/ProductPage/Wardrob
 import { PredefinedValue as WardrobePF } from '~/components/ProductPage/WardrobeProductPage'
 import { ProductComponent as StandPC } from '~/components/ProductPage/StandProductPage'
 import { PredefinedValue as StandPF } from '~/components/ProductPage/StandProductPage'
+import { ProductComponent as TVStandPC } from '~/components/ProductPage/TVStandProductPage'
+import { PredefinedValue as TVStandPF } from '~/components/ProductPage/TVStandProductPage'
+import { ProductComponent as BedsidePC } from '~/components/ProductPage/BedsideProductPage'
+import { PredefinedValue as BedsidePF } from '~/components/ProductPage/BedsideProductPage'
 
-export type CartProductComponent = WardrobePC | StandPC
-export type CartPredefinedValue = WardrobePF | StandPF
+export type CartProductComponent = WardrobePC | StandPC | TVStandPC | BedsidePC
+export type CartPredefinedValue = WardrobePF | StandPF | TVStandPF | BedsidePF
