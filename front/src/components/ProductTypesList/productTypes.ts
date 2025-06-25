40006752e432e8db86d24a893e375407e79b7659
Elizabet Grinciuc <elizabet.grinciuc@bnm.md>
2025-06-25 15:11:06 +0300

    product types href change

--- front/src/components/ProductTypesList/productTypes.ts
+++ front/src/components/ProductTypesList/productTypes.ts
@@ -7,22 +7,22 @@ export const productTypes: ProductTypes[] = [
   {
     name: 'homepage.products.wardrobe',
     link: '/configurator/wardrobe',
-    image: '/renders-landing/wardrobe.png',
+    image: '/renders-landing/wardrobe-1.png',
   },
   {
     name: 'homepage.products.stand',
     link: '/configurator/stand',
-    image: '/renders-landing/stand.png',
+    image: '/renders-landing/stand-1.png',
   },
   {
     name: 'homepage.products.TVstand',
     link: '/configurator/tv-stand',
-    image: '/renders-landing/tv-stand.png',
+    image: '/renders-landing/tv-stand-1.png',
   },
   {
     name: 'homepage.products.bedside',
     link: '/configurator/bedside',
-    image: '/renders-landing/bedside.png'
+    image: '/renders-landing/bedside-1.png'
   },
   {
     name: 'homepage.products.openStand',
