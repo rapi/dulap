f865ef01eacd4e3bb577ddbf89368a553c10dbd2
Elizabet Grinciuc <elizabet.grinciuc@bnm.md>
2025-09-16 10:47:48 +0300

    fixed h1 for SEO

--- front/src/components/BannerContentDulap/BannerContentDulap.tsx
+++ front/src/components/BannerContentDulap/BannerContentDulap.tsx
@@ -7,21 +7,26 @@ export const BannerContent = () => {
   return (
     <div className={classes.heroContent}>
       <div>
-        <h1 className={classes.heroHeader}>
+        <h1 className={classes.visuallyHiddenHeader}>
+          <FormattedMessage
+            id="homepage.banner.header.hidden"
+          />
+        </h1>
+        <h2 className={classes.heroHeader}>
           <FormattedMessage
             id="homepage.banner.header.0"
           />
-        </h1>
+        </h2>
       </div>
       <div>
-        <h1 className={classes.heroTitle}>
+        <h2 className={classes.heroTitle}>
           <FormattedMessage
             id="homepage.banner.preorder.0"
           />
-        </h1>
-        <h1 className={classes.heroTitle}>
+        </h2>
+        <h2 className={classes.heroTitle}>
           <FormattedMessage id="homepage.banner.preorder.1" />
-        </h1>
+        </h2>
       </div>
 
       <div className={classes.heroButtonContainer}>
