3a4857eda87c35cdaea73ca2013d53b73bf621cb
Vasile Grinciuc <raptordias@gmail.com>
2025-09-30 11:54:24 +0300

    update middleware to enforce 301 redirects for Romanian URLs

--- front/middleware.ts
+++ front/middleware.ts
@@ -56,7 +56,7 @@ export function middleware(request: NextRequest) {
   const currentLanguage = newUrl.pathname.split('/')[0]
   if (currentLanguage === 'ro') {
     newUrl.pathname = newUrl.pathname.replace('/ro/', '/')
-    return NextResponse.redirect(newUrl)
+    return NextResponse.redirect(newUrl, { status: 301 })
   } else if (locale !== 'ro' && currentLanguage !== locale) {
     newUrl.pathname = `/${locale}${pathname}`
     const response = NextResponse.redirect(newUrl)
